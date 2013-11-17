18b06770d1110a918b5daa1cb71c0c47b2087d57
rodrimmbdev <rodrimmb@MacBook-Pro-de-Rodrigo.local>
2013-11-17 19:23:57 +0100

    Test in red: closed list should be empty

--- spec/a_star_spec.rb
+++ spec/a_star_spec.rb
@@ -1,8 +1,15 @@
 require 'a_star'
 
 describe 'AStar' do
-	it 'should has open list empty' do
-		aStart = AStar.new
-		expect(aStart.open_list).to eq([])
+	context 'When we dont pass any argument to A star' do
+		it 'should has open list empty' do
+			aStart = AStar.new
+			expect(aStart.open_list).to eq []
+		end
+
+		it 'should has a close list empty' do
+			aStart = AStar.new
+			expect(aStart.closed_list).to eq []
+		end
 	end
 end
