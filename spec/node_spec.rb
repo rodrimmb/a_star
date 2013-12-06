7b97e5319e4ee5a69de4e9669839791c7586b136
rodrimmbdev <rodrimmb@MacBook-Pro-de-Rodrigo.local>
2013-12-06 21:43:23 +0100

    Test in red: The node has a 0 deep when it hasn't parents

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -7,4 +7,11 @@ describe Node do
 
 			expect(node.name).to eq "A"
 		end
+
+		it 'has a 0 deep if it hasn\'t parent' do
+			name = "A"
+			node = Node.new(name)
+
+			expect(node.deep).to eq 0
+		end
 end
