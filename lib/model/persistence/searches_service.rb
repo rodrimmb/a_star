bf07039111909ac462dd4cb291ac2abb6ba4877e
Rodrigo <rodrimmbdev@gmail.com>
2014-05-03 17:37:09 +0200

    Fixed error that allows to add different searches with the same name

--- lib/model/persistence/searches_service.rb
+++ lib/model/persistence/searches_service.rb
@@ -10,7 +10,7 @@ class SearchesService
 
 	def add_search(name)
 		exist = @coll.find("name" => name).to_a
-		if exist
+		if exist.size == 0
 			new_search = { :name => name, :url => make_url(name), :created_on => Time.now }
 			@coll.insert(new_search)
 		end
