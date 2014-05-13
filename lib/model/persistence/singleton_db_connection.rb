d11f0782af2d751518c690fc95cccfa273c86dd3
rodrigo.manjon <rodrimmbdev@gmail.com>
2014-05-13 13:42:15 +0200

    Add multiple parent to a node

--- lib/model/persistence/singleton_db_connection.rb
+++ lib/model/persistence/singleton_db_connection.rb
@@ -2,18 +2,18 @@ class SingletonDbConnection
 
 	@instance = nil
 
-	def self.get_instance(db, collection){
-		if(@instance == nil){
+	def self.get_instance(db, collection)
+		if(@instance == nil)
 			@instance = self.connection(db, collection)
-		}
+		end
 		return @instance
-	}
+	end
 
 	private
 
-	def connection(db, collection){
+	def connection(db, collection)
 		conn = Mongo::Connection.new("localhost", 27017)
 		db = conn.db(db)
 		return db.collection(collection)
-	}
+	end
 end
