725387e64fa0cd5910a7d289f1eb1beae89137fd
Rodrigo <rodrimmbdev@gmail.com>
2014-05-13 19:49:08 +0200

    Test connection to the data base

--- spec/model/singleton_db_connection_spec.rb
+++ spec/model/singleton_db_connection_spec.rb
@@ -3,8 +3,8 @@ require 'model/persistence/singleton_db_connection'
 describe SingletonDbConnection do
 
 	it 'get conection with database' do
-		@collection = SingletonDbConnection.get_instance('mydb','testData')
-		puts @collection.find().to_a
+		@collection = SingletonDbConnection.get_instance('mydb','spec')
+		expect(@collection.find().first["name"]).to eq "test"
 	end
 
 end
