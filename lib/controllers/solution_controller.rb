1f5acb48b6de8d141a05df148c2e1e65154d4df9
Rodrigo <rodrimmbdev@gmail.com>
2014-05-14 20:46:26 +0200

    show the solution of the search in the web

--- lib/controllers/solution_controller.rb
+++ lib/controllers/solution_controller.rb
@@ -4,24 +4,32 @@ require 'json'
 class SolutionController < ApplicationController
 
     get '/:search' do |search|
-    	a_star = AStar.new
-        text = []
-    		
-		collection = SingletonDbConnection.get_instance('mydb','testData')
-		nodes_service = NodesService.new(collection)
-		nodes = NodesHandler.new(search, nodes_service)
-
-		state = nodes.get_first_node()
-		node = Node.new(state)
-		solution = a_star.search(node)
-
-        solution.each do |n|
-            text << n.state.name
-        end
-        json text      
+    	json obtain_solution(search)      
     end
 
     get '/path/:search' do |search|
         
+    end
+
+    private
+
+    def obtain_solution(search)
+        if @searchesService.get_solution(search) == []
+            a_star = AStar.new
+            result = []
+            nodes = NodesHandler.new(search, @nodesService)
+
+            state = nodes.get_first_node()
+            node = Node.new(state)
+            solution = a_star.search(node)
+
+            solution.each do |n|
+                result << n.state.name
+            end
+            @searchesService.set_solution(search,result)
+            result
+        else
+            @searchesService.get_solution(search)
+        end
     end
 end
